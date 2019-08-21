84553929dd9a497c50f8bb16146066a6e24a44e0
Tom Heintzberger <tom.heintzberger@gmail.com>
2019-08-21 13:40:32 +0200

    Conditional script table, latest bootstrap-plus

--- src/main/java/nl/praegus/fitnesse/PraegusPluginFeatureFactory.java
+++ src/main/java/nl/praegus/fitnesse/PraegusPluginFeatureFactory.java
@@ -7,6 +7,7 @@ import fitnesse.responders.ResponderFactory;
 import fitnesse.testsystems.slim.tables.SlimTable;
 import fitnesse.testsystems.slim.tables.SlimTableFactory;
 import nl.praegus.fitnesse.responders.AutoCompleteResponder;
+import nl.praegus.fitnesse.slim.tables.ConditionalScriptTable;
 import nl.praegus.fitnesse.slim.tables.PausingTable;
 
 public class PraegusPluginFeatureFactory extends PluginFeatureFactoryBase {
@@ -16,27 +17,30 @@ public class PraegusPluginFeatureFactory extends PluginFeatureFactoryBase {
         super.registerSlimTables(slimTableFactory);
         LOG.info("[Toolchain Plugin] Registering debug script table. Be sure to add 'pause test fixture' from nl.praegus:toolchain-fixtures as a library");
         add(slimTableFactory, "debug script", PausingTable.class);
+        LOG.info("[Toolchain Plugin] Registering conditional script table.");
+        add(slimTableFactory, "conditional script", ConditionalScriptTable.class);
     }
 
     private void add(SlimTableFactory factory, String key, Class<? extends SlimTable> tableType) {
         factory.addTableType(key, tableType);
-        LOG.info("[Toolchain Plugin] Added Slim table type: " + key + ": " + tableType.getName());
+        //LOG.info("[Toolchain Plugin] Added Slim table type: " + key + ": " + tableType.getName());
     }
 
     @Override
     public void registerResponders(ResponderFactory responderFactory) throws PluginException {
         super.registerResponders(responderFactory);
+        LOG.info("[Toolchain Plugin] Registering AutoCompleteResponder.");
         add(responderFactory, "autoComplete", AutoCompleteResponder.class);
     }
 
     private void add(ResponderFactory factory, String key, Class<? extends Responder> responder) {
         factory.addResponder(key, responder);
-        LOG.info("[Toolchain Plugin] Autoloaded responder " + key + ": " + responder.getName());
+        //LOG.info("[Toolchain Plugin] Autoloaded responder " + key + ": " + responder.getName());
     }
 
     @Override
     public String getDefaultTheme() {
-        LOG.info("[Toolchain Plugin] Setting theme to bootstrap-plus");
+        LOG.info("[Toolchain Plugin] Changing theme to bootstrap-plus");
         return "bootstrap-plus";
     }
 }
