791b1c5b65a4a410be8f196b5337ef52747e79e3
Caitlin <Caitlincoopmans@gmail.com>
2020-01-23 11:01:31 +0100

    Added get set to constructor

--- src/main/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryLine.java
+++ src/main/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryLine.java
@@ -1,6 +1,6 @@
 package nl.praegus.fitnesse.responders.testHistory;
 
-import com.sun.tools.javac.util.List;
+//import com.sun.tools.javac.util.List;
 
 import java.time.LocalDateTime;
 
@@ -9,13 +9,29 @@ public class TestHistoryLine {
     private int numberOfTimesPassed;
     private int numberOfTimesFailed;
     //private List<Boolean> latestResults;
-}
-public TestHistoryLine(String page, int numberOfTimesPassed, int numberOfTimesFailed) {
-    this.page = page;
-    this.numberOfTimesPassed = numberOfTimesPassed;
-    this.numberOfTimesFailed = numberOfTimesFailed;
-}
 
+    //Get+Set Page String
+    public String getPage() {
+        return this.page;
+    }
+    public void SetPage() {
+        this.page = page;
+    }
+    //Get+Set int getNumberOfTimesFailed
+    public int getNumberOfTimesFailed() {
+        return numberOfTimesFailed;
+    }
+    public void setNumberOfTimesFailed(int numberOfTimesFailed) {
+        this.numberOfTimesFailed = numberOfTimesFailed;
+    }
+    //Get+Set int getNumberOfTimesPassed
+    public int getNumberOfTimesPassed() {
+        return numberOfTimesPassed;
+    }
+    public void setNumberOfTimesPassed(int numberOfTimesPassed) {
+        this.numberOfTimesPassed = numberOfTimesPassed;
+    }
+}
 class Result {
     private Boolean result;
     private LocalDateTime resultMoment;
