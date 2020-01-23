1c20f9b8c06d655cebae16f4c266f3ae1d2f54d5
Caitlin <Caitlincoopmans@gmail.com>
2020-01-23 11:16:37 +0100

    Made one set and multiple gets

--- src/main/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryLine.java
+++ src/main/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryLine.java
@@ -10,26 +10,23 @@ public class TestHistoryLine {
     private int numberOfTimesFailed;
     //private List<Boolean> latestResults;
 
-    //Get+Set Page String
+    //Get+Set
+    public TestHistoryLine() {
+        this.page = page;
+        this.numberOfTimesFailed = numberOfTimesFailed;
+        this.numberOfTimesPassed = numberOfTimesPassed;
+    }
+
     public String getPage() {
         return this.page;
     }
-    public void SetPage() {
-        this.page = page;
-    }
-    //Get+Set int getNumberOfTimesFailed
+
     public int getNumberOfTimesFailed() {
         return numberOfTimesFailed;
     }
-    public void setNumberOfTimesFailed(int numberOfTimesFailed) {
-        this.numberOfTimesFailed = numberOfTimesFailed;
-    }
-    //Get+Set int getNumberOfTimesPassed
+
     public int getNumberOfTimesPassed() {
         return numberOfTimesPassed;
-    }
-    public void setNumberOfTimesPassed(int numberOfTimesPassed) {
-        this.numberOfTimesPassed = numberOfTimesPassed;
     }
 }
 class Result {
