c4d8848b25227cbacbd197e17e2fcab964a2cb47
Caitlin <Caitlincoopmans@gmail.com>
2020-01-23 10:46:29 +0100

    Added constructor to TestHistoryLine.java

--- src/main/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryLine.java
+++ src/main/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryLine.java
@@ -8,7 +8,12 @@ public class TestHistoryLine {
     private String page;
     private int numberOfTimesPassed;
     private int numberOfTimesFailed;
-    private List<Boolean> latestResults;
+    //private List<Boolean> latestResults;
+}
+public TestHistoryLine(String page, int numberOfTimesPassed, int numberOfTimesFailed) {
+    this.page = page;
+    this.numberOfTimesPassed = numberOfTimesPassed;
+    this.numberOfTimesFailed = numberOfTimesFailed;
 }
 
 class Result {
