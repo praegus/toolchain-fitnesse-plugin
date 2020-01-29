c7d63ca150c9676430a82bbf487aa4b1742b699b
Caitlin <Caitlincoopmans@gmail.com>
2020-01-29 16:28:01 +0100

    Updated and added tests

--- src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
+++ src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
@@ -1,32 +1,45 @@
 package nl.praegus.fitnesse.responders.testHistory;
+import fitnesse.reporting.history.PageHistory;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestHistoryTest {
-/*
 
     @Test
     public void when_directory_is_null_the_history_lines_are_empty() {
         TestHistory testHistory = new TestHistory(new File(""));
-
-        assertThat (testHistory.getHistoryLines()).isEmpty();
+        assertThat(testHistory.getHistoryLines()).isEmpty();
     }
 
     @Test
     public void When_directory_is_not_null_return_historylines_sorted() {
         TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
         // Compare sorted lists to make sure lists are sorted correctly
-        assertThat(testHistory.getHistoryLines()).extracting("page").containsExactly(
+        assertThat(testHistory.getHistoryLines()).extracting("pageName").containsExactly(
                 "TestSuiteDemo.BackEndTests.T002RetrieveDataFromXas",
                 "TestSuiteDemo.FrontEndTests.T003CreateCourse",
                 "FitNesse.UserGuide.TwoMinuteExample");
-
     }
 
     @Test
     public void When_dateTimeFormatter_is_working_correctly() {
         TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
-        // Compare formatted dates to make sure dateTimeFormatter is working
-        assertThat(testHistory.getHistoryLines().get(0).getMostRecentRunDateFormatted()).isEqualTo("22 jan 2020, 11:43");
-        assertThat(testHistory.getHistoryLines().get(0).getNumberOfTimesFailed()).isEqualTo(3);
+        assertThat(testHistory.getHistoryLines().get(0).getMostRecentRunDate().toString()).isEqualTo("Wed Jan 22 11:43:40 CET 2020");
+    }
+
+    @Test
+    public void Checks_number_of_times_failed(){
+        TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
+        assertThat(testHistory.getHistoryLines().get(0).getNumberOfTimesFailed()).isEqualTo(1);
+    }
+
+    @Test
+    public void Checks_number_of_times_passed(){
+        TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
+        assertThat(testHistory.getHistoryLines().get(0).getNumberOfTimesPassed()).isEqualTo(0);
     }
 
     // Setup method
@@ -39,5 +52,5 @@ public class TestHistoryTest {
             return mockDir;
         }
     }
-*/
+
 }
