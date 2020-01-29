5a4603e09bd2a6e553742ea2d9a79ab0046065ea
Caitlin <Caitlincoopmans@gmail.com>
2020-01-29 11:45:02 +0100

    Added Test When_dateTimeFormatter_is_working_correctly

--- src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
+++ src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
@@ -1,8 +1,14 @@
 package nl.praegus.fitnesse.responders.testHistory;
 
 import org.junit.Test;
-
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.*;
 import java.io.File;
+import java.util.*;
+
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class TestHistoryTest {
@@ -31,8 +37,26 @@ public class TestHistoryTest {
     public void When_directory_is_not_null_return_historylines_unsorted() {
         ClassLoader classLoader = getClass().getClassLoader();
         File mockDir = new File(classLoader.getResource("TestResultDirectory").getFile());
+        //get testhistory lines from mock directory
         TestHistory testHistory = new TestHistory(mockDir);
 
         System.out.println();
     }
+    @Test
+    public void When_dateTimeFormatter_is_working_correctly (){
+        ClassLoader loader = Test.class.getClassLoader();
+        File mockDir = new File(loader.getResource("TestResultDirectory").getFile());
+        TestHistory testHistory = new TestHistory(mockDir);
+        List<TestHistoryLine> testHistoryList = testHistory.getHistoryLines();
+
+        List<LocalDateTime> unformattedDates = testHistoryList.stream().map(TestHistoryLine::getLastRun).collect(Collectors.toList());
+        LocalDateTime unformattedDate = unformattedDates.get(0);
+
+        List<String> formattedDatesMock = testHistoryList.stream().map(TestHistoryLine::getFormattedDate).collect(Collectors.toList());
+        String formattedDateTest = unformattedDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm"));
+
+        assertThat(formattedDateTest).isEqualTo(formattedDatesMock.get(0));
+        System.out.println();
+    }
+
 }
