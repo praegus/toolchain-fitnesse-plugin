766e12c5f4b66bb5149f709243cfd66adc908eca
alex pennings <apennings@roc-dev.com>
2020-01-29 11:26:00 +0100

    created getmockdir in test

--- src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
+++ src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
@@ -7,32 +7,33 @@ import java.io.File;
 import static org.assertj.core.api.Assertions.assertThat;
 public class TestHistoryTest {
 
+    private File getMockDir(String DirName){
+        if (DirName == ""){
+            File mockDir = new File("");
+            return mockDir;
+        }else {
+            File mockDir = new File(getClass().getClassLoader().getResource(DirName).getFile());
+            return mockDir;
+        }
+
+    }
+
 
     @Test
     public void when_directory_is_null_the_history_lines_are_empty() {
-        File mockDir = new File("");
-        TestHistory testHistory = new TestHistory(mockDir);
+
+        TestHistory testHistory = new TestHistory(getMockDir(""));
         assert (testHistory.getHistoryLines()).isEmpty();
     }
 
     @Test
     public void When_directory_is_not_null_return_historylines_sorted() {
-        File mockDir = new File(getClass().getClassLoader().getResource("TestResultDirectory").getFile());
 
-        TestHistory testHistory = new TestHistory(mockDir);
+        TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
 
         assertThat(testHistory.getHistoryLines()).extracting("page").containsExactly(
                 "TestSuiteDemo.BackEndTests.T002RetrieveDataFromXas",
                 "TestSuiteDemo.FrontEndTests.T003CreateCourse",
                 "FitNesse.UserGuide.TwoMinuteExample");
-    }
-
-    @Test
-    public void When_directory_is_not_null_return_historylines_unsorted() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File mockDir = new File(classLoader.getResource("TestResultDirectory").getFile());
-        TestHistory testHistory = new TestHistory(mockDir);
-
-        System.out.println();
     }
 }
