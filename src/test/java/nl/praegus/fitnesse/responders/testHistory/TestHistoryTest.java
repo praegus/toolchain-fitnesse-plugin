c610bf448b9377d18369f74cc79169af2fe22873
alex pennings <apennings@roc-dev.com>
2020-01-30 11:28:54 +0100

    added test

--- src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
+++ src/test/java/nl/praegus/fitnesse/responders/testHistory/TestHistoryTest.java
@@ -1,5 +1,4 @@
 package nl.praegus.fitnesse.responders.testHistory;
-import fitnesse.reporting.history.PageHistory;
 import org.junit.Test;
 
 import java.io.File;
@@ -17,7 +16,7 @@ public class TestHistoryTest {
     @Test
     public void When_directory_is_not_null_return_historylines_sorted() {
         TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
-        // Compare sorted lists to make sure lists are sorted correctly
+
         assertThat(testHistory.getHistoryLines()).extracting("pageName").containsExactly(
                 "TestSuiteDemo.BackEndTests.T002RetrieveDataFromXas",
                 "TestSuiteDemo.FrontEndTests.T003CreateCourse",
@@ -25,33 +24,39 @@ public class TestHistoryTest {
     }
 
     @Test
+    public void When_directory_is_not_null_return_history_lines_unsorted(){
+        TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
+
+        assertThat(testHistory.getPageNames()).contains("TestSuiteDemo.FrontEndTests.T003CreateCourse",
+                        "TestSuiteDemo.FrontEndTests.T003CreateCourse",
+                        "FitNesse.UserGuide.TwoMinuteExample");
+    }
+
+    @Test
     public void When_dateTimeFormatter_is_working_correctly() {
         TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
-        // Compare formatted dates to make sure dateTimeFormatter is working
+
         assertThat(testHistory.getHistoryLines().get(0).getMostRecentRunDate().toString()).isEqualTo("Wed Jan 22 11:43:40 CET 2020");
     }
 
     @Test
     public void Checks_number_of_times_failed(){
         TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
+
         assertThat(testHistory.getHistoryLines().get(0).getNumberOfTimesFailed()).isEqualTo(1);
     }
 
     @Test
     public void Checks_number_of_times_passed(){
         TestHistory testHistory = new TestHistory(getMockDir("TestResultDirectory"));
+
         assertThat(testHistory.getHistoryLines().get(0).getNumberOfTimesPassed()).isEqualTo(0);
     }
 
     // Setup method
     private File getMockDir(String DirName){
-        if (DirName == "") {
-            File mockDir = new File("");
-            return mockDir;
-        } else {
             File mockDir = new File(getClass().getClassLoader().getResource(DirName).getFile());
             return mockDir;
-        }
     }
 
 }
