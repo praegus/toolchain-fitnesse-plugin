57e42a7a38b4bb190db145c6840e230299899c88
Caitlin <Caitlincoopmans@gmail.com>
2020-03-04 08:29:23 +0100

    Comment 5

--- src/test/java/nl/praegus/fitnesse/responders/allTags/TestWikiPageDummy.java
+++ src/test/java/nl/praegus/fitnesse/responders/allTags/TestWikiPageDummy.java
@@ -1,24 +1,24 @@
 package nl.praegus.fitnesse.responders.allTags;
 
-import fitnesse.util.Clock;
-import fitnesse.wiki.*;
+import fitnesse.wiki.PageData;
+import fitnesse.wiki.WikiPage;
+import fitnesse.wiki.WikiPageProperty;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-class TestWikiPageDummy extends BaseWikiPage {
+class TestWikiPageDummy extends fitnesse.wiki.WikiPageDummy {
     private final List<WikiPage> children;
     private PageData pageData;
 
     public TestWikiPageDummy(String name, String content, WikiPage parent, WikiPageProperty property) {
-        super(name, parent);
+        //super(name, parent);
         this.pageData = new PageData(content, property);
         this.children = Collections.emptyList();
     }
 
     public TestWikiPageDummy(String name, String content, WikiPage parent, WikiPageProperty property, List<WikiPage> children) {
-        super(name, parent);
+ //       super(name, parent);
         this.pageData = new PageData(content, property);
         this.children = children;
     }
@@ -27,38 +27,37 @@ class TestWikiPageDummy extends BaseWikiPage {
         return this.pageData;
     }
 
-    public Collection<VersionInfo> getVersions() {
-        return Collections.emptySet();
-    }
+//    public Collection<VersionInfo> getVersions() {
+//        return Collections.emptySet();
+//    }
 
-    public VersionInfo commit(PageData data) {
-        this.pageData = data;
-        return new VersionInfo("mockVersionName", "mockAuthor", Clock.currentDate());
-    }
+//    public VersionInfo commit(PageData data) {
+//        this.pageData = data;
+//        return new VersionInfo("mockVersionName", "mockAuthor", Clock.currentDate());
+//    }
 
     public List<WikiPage> getChildren() {
         return children;
     }
 
-    public WikiPage getVersion(String versionName) {
-        return this;
-    }
+//    public WikiPage getVersion(String versionName) {
+//        return this;
+//    }
+//
+//    public String getHtml() {
+//        return String.format("<em>%s</em>", this.pageData.getContent());
+//    }
+//
+//    public void removeChildPage(String name) {}
+//
+//    public String getVariable(String name) {
+//        return null;
+//    }
+//
+//    public WikiPage addChildPage(String name) { return null; }
 
-    public String getHtml() {
-        return String.format("<em>%s</em>", this.pageData.getContent());
-    }
-
-    public void removeChildPage(String name) {
-    }
-
-    public String getVariable(String name) {
-        return null;
-    }
-
-    public WikiPage addChildPage(String name) { return null; }
-
-    public WikiPage getChildPage(String name) {
-        return null;
-    }
+  //  public WikiPage getChildPage(String name) {
+    //    return null;
+ //   }
 }
 
