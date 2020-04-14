2163da4ff53a4afed6176f4598640a9566d6d051
alex pennings <apennings@roc-dev.com>
2020-04-14 14:02:03 +0200

    rewriten ToolTips.java to read fixture tooltips and bootstrap tooltips

--- src/main/java/nl/praegus/fitnesse/responders/ToolTip/ToolTips.java
+++ src/main/java/nl/praegus/fitnesse/responders/ToolTip/ToolTips.java
@@ -1,6 +1,7 @@
 package nl.praegus.fitnesse.responders.ToolTip;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 import java.util.jar.JarEntry;
@@ -10,76 +11,64 @@ import java.util.stream.Collectors;
 public class ToolTips {
 
     private List<String> toolTips = new ArrayList<>();
-    public String fixturePath = "/fixtures";
-    public String toolchainPath = "/plugins";
+    public String tooltipTargetDirectory = "/TooltipData";
+    public String bootstrapTargetDirectory =  "/TooltipData/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
-    private void addToolTips() {
-        List<JarFile> fixtures = getFixtures();
+    private void addToolTips(){
+        File[] dirs = new File(System.getProperty("user.dir")+ tooltipTargetDirectory).listFiles();
+        for (File dir : dirs){
+            // check if File in list is directory so we wont try to listfiles from a file
+            if(dir.isDirectory()){
+                for (File tooltipFile : new File(dir.getPath()).listFiles()) {
+                    //check if the file is actual tooltips.txt file so we arent reading any other txt files
+                    if (tooltipFile.getName().toLowerCase().equals("tooltips.txt")) {
+                        try {
+                            URL TooltipsURL = new URL("file:///" + dir.getPath() + "/tooltips.txt");
 
-        for (JarFile fixture : fixtures) {
-            //for each fixture check if tooltips.txt is present and then parse it to jar entry
-            Optional<JarEntry> tooltips = fixture.stream().filter(name -> name.getName().toLowerCase().contains("tooltips.txt")).findFirst();
-            //check if tooltips is present so url.openstream doesnt make a null error
-            if (tooltips.isPresent()) {
-                try {
-                    URL url = new URL("jar:file:" + fixture.getName() + "!/" + tooltips.get().getName());
-                    InputStream inputStream = url.openStream();
-
-                    InputStreamReader reader = new InputStreamReader(inputStream);
-                    BufferedReader bufferedReader = new BufferedReader(reader);
-                    // for each line, which is a tooltip add it to the tooltip list
-                    toolTips.addAll(bufferedReader.lines().collect(Collectors.toList()));
-                } catch (IOException e) {
-                    System.out.println("couldn't parse tooltips in fixture " + fixture.getName());
+                            InputStream inputStream = TooltipsURL.openStream();
+                            InputStreamReader reader = new InputStreamReader(inputStream);
+                            BufferedReader bufferedReader = new BufferedReader(reader);
+                            //for each line, which is a tooltip add it to the tooltip list
+                            toolTips.addAll(bufferedReader.lines().collect(Collectors.toList()));
+                        } catch (IOException e) {
+                            System.out.println("couldn't parse tooltips for fixture " + dir.getName());
+                        }
+                    }
                 }
             }
         }
-        addBootstrapTooltips();
     }
-
-    private List<JarFile> getFixtures() {
-        // get all content within the fixture map and check if they arent null
-        List<File> directoryContent = Arrays.asList(Objects.requireNonNull(new File(System.getProperty("user.dir") + fixturePath).listFiles()));
-        // get all jars within the fixture map and parse it to JarFile
-        List<JarFile> fixtures = directoryContent.stream().filter(name -> name.toString().contains(".jar")).map(o -> {
+    private void addBootstrapTooltips(){
+        File BootstrapTooltips = new File(System.getProperty("user.dir") + bootstrapTargetDirectory);
+        // check if file exists so we wont get an error
+        if (BootstrapTooltips.exists()){
             try {
-                return new JarFile(o);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }).collect(Collectors.toList());
-        return fixtures;
-    }
+                URL BootstrapTooltipsURL = new URL("file:///" + BootstrapTooltips.getPath());
 
-    private void addBootstrapTooltips() {
-        // get tooltips.txt from the bootstrap plugin
-        Optional<File> toolChain = Arrays.stream(Objects.requireNonNull(new File(System.getProperty("user.dir") + toolchainPath).listFiles())).filter(name -> name.toString().toLowerCase().contains("toolchain-fitnesse-plugin")).findFirst();
-        // check if toochain is a file we can read so inputStream doesnt give a null error
-        if (toolChain.get().isFile()) {
-            try {
-                URL url = new URL("jar:file:" + toolChain.get().getPath() + "!/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt");
-                InputStream inputStream = url.openStream();
+                InputStream inputStream = BootstrapTooltipsURL.openStream();
                 InputStreamReader reader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(reader);
-                // for each line, which is a tooltip add it to the tooltip list
+                //for each line, which is a tooltip add it to the tooltip list
                 toolTips.addAll(bufferedReader.lines().collect(Collectors.toList()));
-            } catch (IOException e) {
+            }catch (IOException e){
                 System.out.println("couldn't parse bootstrap tooltips");
             }
         }
+
     }
 
     public String getToolTip() {
-        addToolTips();
 
+        addBootstrapTooltips();
+        //after bootstrap tooltips have been loaded, check if tooltips actuallly contains tooltips. if not return null so bootstrap tooltips will be loaded in JS.
         if (toolTips.size() != 0) {
+            addToolTips();
             Random rand = new Random();
             int pickedTip = rand.nextInt(toolTips.size());
 
             return toolTips.get(pickedTip);
         } else {
-            return null;
+            return "";
         }
     }
 }
