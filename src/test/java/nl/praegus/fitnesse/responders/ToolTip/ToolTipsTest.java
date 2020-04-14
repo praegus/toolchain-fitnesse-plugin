06bed1197959559425eb96e36cb5b7fdfce40afd
alex pennings <apennings@roc-dev.com>
2020-04-14 14:19:00 +0200

    added test resources and test

--- src/test/java/nl/praegus/fitnesse/responders/ToolTip/ToolTipsTest.java
+++ src/test/java/nl/praegus/fitnesse/responders/ToolTip/ToolTipsTest.java
@@ -2,88 +2,62 @@ package nl.praegus.fitnesse.responders.ToolTip;
 
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ToolTipsTest {
 
     @Test
-    public void check_if_ToolTip_is_not_null_when_TooltipLists_are_given() {
+    public void check_if_ToolTip_is_not_null_when_fixture_tooltips_and_bootstrap_tooltips_are_given() {
         ToolTips toolTips = new ToolTips();
-        toolTips.fixturePath = "/src/test/resources/ToolTips/fixtures";
-        toolTips.toolchainPath = "/src/test/resources/ToolTips/toolchain";
+        toolTips.tooltipTargetDirectory = "/src/test/resources/Tooltip/TooltipData_default";
+        toolTips.bootstrapTargetDirectory = "/src/test/resources/Tooltip/TooltipData_default/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
         String receivedResult = toolTips.getToolTip();
 
-        assertThat(receivedResult).isNotEmpty();
+        assertThat(receivedResult).isNotEqualTo("");
+    }
+    @Test
+    public void check_if_ToolTip_is_not_null_when_fixtureTooltips_are_null_and_bootstrapTooltips_are_given() {
+        ToolTips toolTips = new ToolTips();
+        toolTips.tooltipTargetDirectory = "/src/test/resources/Tooltip/TooltipData_noFixtureTooltip";
+        toolTips.bootstrapTargetDirectory = "/src/test/resources/Tooltip/TooltipData_noFixtureTooltip/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
+        String receivedResult = toolTips.getToolTip();
+
+        assertThat(receivedResult).isNotEqualTo("");
     }
 
     @Test
-    public void check_if_ToolTip_is_null_when_ToolTipslist_is_empty() {
+    public void check_if_returns_null_if_ToolTipList_is_empty(){
         ToolTips toolTips = new ToolTips();
-        toolTips.fixturePath = "/src/test/resources/ToolTips/fixtures_null";
-        toolTips.toolchainPath = "/src/test/resources/ToolTips/toolchain_null";
+        toolTips.tooltipTargetDirectory = "/src/test/resources/Tooltip/TooltipData_empty_txt";
+        toolTips.bootstrapTargetDirectory = "/src/test/resources/Tooltip/TooltipData_empty_txt/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
         String receivedResult = toolTips.getToolTip();
 
-        assertThat(receivedResult).isNull();
-
+        assertThat(receivedResult).isEqualTo("");
     }
 
     @Test
-    public void Check_if_jars_are_not_found_returns_null() {
+    public void check_if_returns_null_if_there_is_no_txt_given(){
         ToolTips toolTips = new ToolTips();
-        toolTips.fixturePath = "/src/test/resources/ToolTips";
-        toolTips.toolchainPath = "/src/test/resources/ToolTips";
+        toolTips.tooltipTargetDirectory = "/src/test/resources/Tooltip/TooltipData_no_txt";
+        toolTips.bootstrapTargetDirectory = "/src/test/resources/Tooltip/TooltipData_no_txt/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
         String receivedResult = toolTips.getToolTip();
 
-        assertThat(receivedResult).isNull();
+        assertThat(receivedResult).isEqualTo("");
     }
 
     @Test
-    public void Check_if_only_bootstrap_tips_are_found_returns_correct_tooltips() {
+    public void check_return_null_if_bootstrapToolTips_are_missing(){
         ToolTips toolTips = new ToolTips();
-        toolTips.fixturePath = "/src/test/resources/ToolTips";
-        toolTips.toolchainPath = "/src/test/resources/ToolTips/toolchain";
+        toolTips.tooltipTargetDirectory = "/src/test/resources/Tooltip/TooltipData_default";
+        toolTips.bootstrapTargetDirectory = "/src/test/resources/Tooltip/TooltipData_no_txt/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
         String receivedResult = toolTips.getToolTip();
-        List expectedResult = getBootStraptoolTipList();
 
-        assertThat(expectedResult).contains(receivedResult);
-
-    }
-
-    @Test
-    public void Check_if_only_fixture_tips_are_found_returns_correct_tooltips() {
-        ToolTips toolTips = new ToolTips();
-        toolTips.fixturePath = "/src/test/resources/ToolTips/fixtures";
-        toolTips.toolchainPath = "/src/test/resources/ToolTips";
-
-        String receivedResult = toolTips.getToolTip();
-        List expectedResult = getFixtureTooltipList();
-
-        assertThat(expectedResult).contains(receivedResult);
-    }
-
-
-    private List<String> getFixtureTooltipList() {
-        InputStreamReader inputStreamReader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("ToolTips/ToolTipData/FixtureToolTips.txt"));
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-        return bufferedReader.lines().collect((Collectors.toList()));
-    }
-
-    private List<String> getBootStraptoolTipList() {
-        InputStreamReader inputStreamReader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("ToolTips/ToolTipData/BootStrapToolTips.txt"));
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-        return bufferedReader.lines().collect((Collectors.toList()));
+        assertThat(receivedResult).isEqualTo("");
     }
 
 }
