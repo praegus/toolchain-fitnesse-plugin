dfe2d20189ed862b0d0d00542cbc8e070b2cf726
alex pennings <apennings@roc-dev.com>
2020-04-16 16:43:44 +0200

    rewriten bootstrap tooltips method so no Javascript fallback is needed

--- src/main/java/nl/praegus/fitnesse/responders/ToolTip/ToolTips.java
+++ src/main/java/nl/praegus/fitnesse/responders/ToolTip/ToolTips.java
@@ -1,19 +1,12 @@
 package nl.praegus.fitnesse.responders.ToolTip;
-
-import jdk.internal.loader.Resource;
-
 import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.jar.JarFile;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ToolTips {
     private List<String> toolTips = new ArrayList<>();
     public String tooltipTargetDirectory = "/TooltipData";
-    public String bootstrapTargetDirectory = "/TooltipData/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt";
 
     private void addToolTips() {
         File[] dirs = new File(System.getProperty("user.dir") + tooltipTargetDirectory).listFiles();
@@ -67,13 +60,17 @@ public class ToolTips {
     }
 
     private void addBootstrapTooltips() {
+        String[] classPaths = System.getProperty("java.class.path").split(";");
+        String toolchainPath = null;
+        for (String classpath:classPaths) {
+            if (classpath.contains("toolchain-fitnesse-plugin")){
+                toolchainPath = classpath;
+            }
+        }
 
-        File bootstrapTooltips = new File(System.getProperty("user.dir") + bootstrapTargetDirectory);
-        // check if file exists so we wont get an error
         try {
-            if (bootstrapTooltips.exists()) {
-
-                URL BootstrapTooltipsURL = new URL("file:///" + bootstrapTooltips.getPath());
+            if (toolchainPath != null) {
+                URL BootstrapTooltipsURL = new URL("jar:file:" + toolchainPath + "!/fitnesse/resources/bootstrap-plus/txt/toolTipData.txt");
 
                 InputStream inputStream = BootstrapTooltipsURL.openStream();
                 InputStreamReader reader = new InputStreamReader(inputStream);
@@ -81,17 +78,12 @@ public class ToolTips {
                 //for each line, which is a tooltip add it to the tooltip list
                 toolTips.addAll(bufferedReader.lines().collect(Collectors.toList()));
             }
-        } catch (IOException e) {
-            System.out.println("couldn't parse bootstrap tooltips");
-        }
-
-
+            } catch(IOException e){
+                System.out.println("couldn't parse bootstrap tooltips");
+            }
     }
 
     public String getToolTip(String cookieIsValid) throws IOException {
-
-
-
         if (cookieIsValid.equals("false")){
             addBootstrapTooltips();
             addToolTips();
@@ -99,7 +91,6 @@ public class ToolTips {
         }else{
             toolTips = getFileCache(System.getProperty("user.dir")+tooltipTargetDirectory+"/tooltipCache.txt");
         }
-
         //after bootstrap tooltips have been loaded, check if tooltips actuallly contains tooltips. if not return null so bootstrap tooltips will be loaded in JS.
 
         if (toolTips.size() != 0) {
