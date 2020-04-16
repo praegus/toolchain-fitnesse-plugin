4775381c0e2ad40fdfb02df116403ca54d973282
Alex <s1129351@student.windesheim.nl>
2020-04-16 15:52:17 +0200

    US 57 - Responder returns HTML page with Symlink information

--- src/main/java/nl/praegus/fitnesse/responders/symbolicLink/SymbolicLinkResponder.java
+++ src/main/java/nl/praegus/fitnesse/responders/symbolicLink/SymbolicLinkResponder.java
@@ -4,6 +4,8 @@ import fitnesse.FitNesseContext;
 import fitnesse.authentication.SecureOperation;
 import fitnesse.authentication.SecureReadOperation;
 import fitnesse.authentication.SecureResponder;
+import fitnesse.html.template.HtmlPage;
+import fitnesse.html.template.PageTitle;
 import fitnesse.http.Request;
 import fitnesse.http.Response;
 import fitnesse.http.SimpleResponse;
@@ -12,25 +14,30 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Set;
 
+import static fitnesse.wiki.WikiPageProperty.SUITES;
 import static nl.praegus.fitnesse.responders.WikiPageHelper.loadPage;
 
 public class SymbolicLinkResponder implements SecureResponder {
     public JSONArray symboliclinkArray = new JSONArray();
+    private PageData pageData;
+
     @Override
     public Response makeResponse(FitNesseContext fitNesseContext, Request request) throws Exception {
+        WikiSourcePage sourcePage = new WikiSourcePage(loadPage(fitNesseContext, request.getResource(), request.getMap()));
         WikiPage wikiPage = loadPage(fitNesseContext, request.getResource(), request.getMap());
-
-        return makeTagResponse(wikiPage);
+        pageData = wikiPage.getData();
+        return makeTagResponse(wikiPage, fitNesseContext, request);
     }
 
-    public SimpleResponse makeTagResponse(WikiPage wikiPage) throws UnsupportedEncodingException {
+    public SimpleResponse makeTagResponse(WikiPage wikiPage, FitNesseContext fitNesseContext, Request request) throws UnsupportedEncodingException {
         SimpleResponse response = new SimpleResponse();
         response.setMaxAge(0);
         response.setStatus(200);
-        response.setContentType("application/json");
-        response.setContent(getSymlink(wikiPage).toString());
+        response.setContentType("text/html");
+//        response.setContent(getSymlink(wikiPage).toString());
+        response.setContent(createHTMLPage(fitNesseContext, request, wikiPage));
+
         return response;
     }
 
@@ -40,8 +47,7 @@ public class SymbolicLinkResponder implements SecureResponder {
 
     private JSONArray getSymlinkHelper(WikiPage wikiPage) {
         // Get Symlinks for the wiki page
-        PageData data = wikiPage.getData();
-        WikiPageProperty symLinksProperty = data.getProperties().getProperty(SymbolicPage.PROPERTY_NAME);
+        WikiPageProperty symLinksProperty = pageData.getProperties().getProperty(SymbolicPage.PROPERTY_NAME);
 
         // Loop to the names
         if (symLinksProperty != null) {
@@ -63,6 +69,22 @@ public class SymbolicLinkResponder implements SecureResponder {
         symboliclinkInformation.put("linkName", name);
         symboliclinkInformation.put("linkPath", symLinksProperty.get(name));
         return symboliclinkInformation;
+    }
+
+    private String createHTMLPage(FitNesseContext fitNesseContext, Request request, WikiPage wikiPage) {
+        HtmlPage html = fitNesseContext.pageFactory.newPage();
+        html.setNavTemplate("viewNav");
+        html.setMainTemplate("symlinks");
+        html.put("viewLocation", request.getResource());
+        html.put("symlinks", getSymlink(wikiPage));
+        html.setTitle("Symbolic links: " + request.getResource());
+
+        String tags = "";
+        if(pageData != null)  {
+            tags = pageData.getAttribute(SUITES);
+        }
+        html.setPageTitle(new PageTitle("Symbolic links", PathParser.parse(request.getResource()), tags));
+        return html.html(request);
     }
 
     @Override
