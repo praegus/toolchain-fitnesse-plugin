844e7966220d649d9e778965cf7845baf38abe0b
Caitlin <Caitlincoopmans@gmail.com>
2020-04-30 13:53:58 +0200

    Fixed test

--- src/test/java/nl/praegus/fitnesse/symbols/MavenProjectVersions/MavenProjectVersionsSymbolTest.java
+++ src/test/java/nl/praegus/fitnesse/symbols/MavenProjectVersions/MavenProjectVersionsSymbolTest.java
@@ -2,8 +2,6 @@ package nl.praegus.fitnesse.symbols.MavenProjectVersions;
 
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MavenProjectVersionsSymbolTest {
@@ -40,15 +38,11 @@ public class MavenProjectVersionsSymbolTest {
         assertThat(receivedValue).isEqualTo("Up-to-date");
     }
 
-//    @Test
-//    public void testing_toTarget() {
-//        MavenProjectVersionsSymbol MavenVersionSymbol = new MavenProjectVersionsSymbol();
-//        TestWikiPageDummyMavenVersions TestWikiPageDummyMavenVersions = new TestWikiPageDummyMavenVersions("dummyPage", "", new WikiPageDummy(),new WikiPageProperty());
-//        WikiSourcePage testWikiSourcePage = new WikiSourcePage(TestWikiPageDummyMavenVersions);
-//        Symbol symbol = new Symbol(new SymbolType("dwa"), "adaw", 1, 1);
-//        Translator translator = new Translator(testWikiSourcePage, symbol);
-//        System.out.println(MavenVersionSymbol.toTarget(translator, symbol);
-//
-//    }
-}
+    @Test
+    public void check_if_pom_is_not_found() {
+        MavenProjectVersionsSymbol MavenVersionSymbol = new MavenProjectVersionsSymbol();
+        String receivedValue = MavenVersionSymbol.toTarget(null, null);
+        assertThat(receivedValue).contains("POM not found!");
+    }
 
+}
